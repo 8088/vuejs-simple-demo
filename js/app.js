2efa15e663fcc3a00bb70d4295431f8398618420
刘军立 <flasher.jun@gmail.com>
2017-06-06 18:22:41 +0800

    creat demo

---
+++ js/app.js
@@ -0,0 +1,88 @@
+
+var app = new Vue({
+
+    el: '#app',
+
+    data: {
+        question: '',
+        answer: 'I cannot give you an answer until you ask a question!',
+        url: 'http://www.vipkid.com.cn'
+    },
+
+    created: function () {
+        //this.fetchData()
+    },
+
+    watch: {
+        // 如果 question 发生改变，这个函数就会运行
+        question: function (newQuestion) {
+            this.answer = 'Waiting for you to stop typing...'
+            this.getAnswer()
+        }
+    },
+
+    filters: {
+        truncate: function (v) {
+            var newline = v.indexOf('\n')
+            return newline > 0 ? v.slice(0, newline) : v
+        },
+        formatDate: function (v) {
+            return v.replace(/T|Z/g, ' ')
+        }
+    },
+
+    methods: {
+        fetchData: function () {
+            var xhr = new XMLHttpRequest()
+            var self = this
+            xhr.open('GET', apiURL + self.currentBranch)
+            xhr.onload = function () {
+                self.commits = JSON.parse(xhr.responseText)
+                console.log(self.commits[0].html_url)
+            }
+            xhr.send()
+        },
+
+        // _.debounce 是一个通过 lodash 限制操作频率的函数。
+        // 在这个例子中，我们希望限制访问yesno.wtf/api的频率
+        // ajax请求直到用户输入完毕才会发出
+        // 学习更多关于 _.debounce function (and its cousin
+        // _.throttle), 参考: https://lodash.com/docs#debounce
+        loadData: _.debounce(
+            function () {
+                var vm = this
+                if (this.question.indexOf('?') === -1) {
+                    vm.answer = 'Questions usually contain a question mark. ;-)'
+                    return
+                }
+                vm.answer = 'Thinking...'
+                axios.get('https://yesno.wtf/api')
+                    .then(function (response) {
+                        vm.answer = _.capitalize(response.data.answer)
+                    })
+                    .catch(function (error) {
+                        vm.answer = 'Error! Could not reach the API. ' + error
+                    })
+            },
+            500
+        )
+    },
+    render(h) {
+        return h(
+            'iframe', {
+                style: {
+                    width: '100%',
+                    height: '100%',
+                    border: 'none'
+                },
+                attrs: {
+                    src: this.url
+                },
+            }
+        );
+    }
+})
+
+window.addEventListener('popstate', () => {
+    app.currentRoute = window.location.pathname
+})
